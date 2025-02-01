3f6a9db42d08915b6425c9c80a659971c940c8ff
ToxicStoxm <dominik0815oberlechner@gmail.com>
2025-02-01 12:22:08 +0100

    Updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,11 @@ module YAJL {
     requires static lombok;
     requires org.jetbrains.annotations;
     requires java.desktop;
+
+    exports com.toxicstoxm.YAJL;
+    exports com.toxicstoxm.YAJL.tools;
+    exports com.toxicstoxm.YAJL.level;
+    exports com.toxicstoxm.YAJL.area;
+    exports com.toxicstoxm.YAJL.placeholders;
+    exports com.toxicstoxm.YAJL.config;
 }
