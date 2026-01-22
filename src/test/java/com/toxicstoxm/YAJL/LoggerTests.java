92a04504ff7c67a414e1654c725dafc62eb46f63
Dominik <dominik@crafti-servi.com>
2026-01-22 18:20:48 +0100

    Simple tests for new implementation

--- src/test/java/com/toxicstoxm/YAJL/LoggerTests.java
+++ src/test/java/com/toxicstoxm/YAJL/LoggerTests.java
@@ -3,16 +3,30 @@ package com.toxicstoxm.YAJL;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.List;
 
 public class LoggerTests {
-    private final Logger logger = new Logger(getClass());
+    private static final Logger logger = LoggerManager.getLogger(LoggerTests.class);
+    private static final Logger virtuallogger = LoggerManager.getVirtualLogger("SomeVirutalArea");
 
     @Test
     public void generalTest() {
         LoggerManager.getInstance(new File("/home/dominik/Downloads/test.yaml"));
 
-        System.out.println(LoggerManager.getSettings().getTest());
-
         logger.log("Hello World!");
+        virtuallogger.log("Hello World!");
+
+        boolean isFiltered = LoggerManager.getSettings().getLogFilter().isFiltered("Hello World!");
+        System.out.println(isFiltered);
+
+        isFiltered = LoggerManager.getSettings().getLogFilter().isFiltered("Hello World!");
+        System.out.println(isFiltered);
+
+        LoggerManager.configure()
+                .addLogFilterPattern("Hello")
+                .done();
+
+        isFiltered = LoggerManager.getSettings().getLogFilter().isFiltered("Hello World!");
+        System.out.println(isFiltered);
     }
 }
