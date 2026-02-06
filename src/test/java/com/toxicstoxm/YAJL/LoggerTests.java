f663a820039966c51a71bbe8661ab2d7ef8b83ef
Dominik <dominik@crafti-servi.com>
2026-02-06 10:25:30 +0100

    Add tests for log arguments with {} placeholders

--- src/test/java/com/toxicstoxm/YAJL/LoggerTests.java
+++ src/test/java/com/toxicstoxm/YAJL/LoggerTests.java
@@ -20,6 +20,31 @@ public class LoggerTests {
     private static final Logger logger = LoggerManager.getLogger(LoggerTests.class);
     private static final Logger virtuallogger = LoggerManager.getVirtualLogger("SomeVirutalArea");
 
+    private void configure(PrintStream ps) {
+        LoggerManager.configure()
+                .addOutput(ps)
+                .logMessageLayout("A {message}")
+                .done();
+    }
+
+    private static class LogCapture implements AutoCloseable {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final PrintStream ps = new PrintStream(out);
+
+        String read() {
+            return out.toString();
+        }
+
+        void reset() {
+            out.reset();
+        }
+
+        @Override
+        public void close() {
+            ps.close();
+        }
+    }
+
     @BeforeEach
     public void setup() {
         LoggerManager.resetSettings();
@@ -220,6 +245,226 @@ public class LoggerTests {
 
         logger.info("Y");
         assertEquals("B Y\n", bs.toString());
+    }
+
+    @Test
+    void noArgs_doesNotFormat() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello!");
+            assertEquals("A Hello!\n", cap.read());
+        }
+    }
+
+    @Test
+    void singlePlaceholder_isReplaced() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello! {}", "Hello");
+            assertEquals("A Hello! Hello\n", cap.read());
+        }
+    }
+
+    @Test
+    void multilineMessage_isSplitAndFormatted() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello!\n{}", "Hello");
+
+            assertEquals(
+                    """
+                            A Hello!
+                            A Hello
+                            """,
+                    cap.read()
+            );
+        }
+    }
+
+    @Test
+    void emptyLines_areSkipped() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello!\n\n\n{}", "Hello");
+
+            assertEquals(
+                    """
+                            A Hello!
+                            A Hello
+                            """,
+                    cap.read()
+            );
+        }
+    }
+
+    @Test
+    void missingArgs_leavePlaceholders() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello! {}{}", "Hello");
+
+            assertEquals("A Hello! Hello{}\n", cap.read());
+        }
+    }
+
+    @Test
+    void placeholdersWithoutArgs_areUntouched() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello! {}{}");
+
+            assertEquals("A Hello! {}{}\n", cap.read());
+        }
+    }
+
+    @Test
+    void nullArgument_isRenderedAsNull() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Value = {}", (Object) null);
+
+            assertEquals("A Value = null\n", cap.read());
+        }
+    }
+
+    @Test
+    void extraArguments_areIgnored() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello {}", "A", "B", "C");
+
+            assertEquals("A Hello A\n", cap.read());
+        }
+    }
+
+    @Test
+    void argsWithoutPlaceholders_doNothing() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("Hello", "A", "B");
+
+            assertEquals("A Hello\n", cap.read());
+        }
+    }
+
+    @Test
+    void adjacentPlaceholders_areHandledCorrectly() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("{}{}", "A", "B");
+
+            assertEquals("A AB\n", cap.read());
+        }
+    }
+
+    @Test
+    void placeholderAtStartAndEnd() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("{} middle {}", "A", "B");
+
+            assertEquals("A A middle B\n", cap.read());
+        }
+    }
+
+    @Test
+    void unmatchedBraces_areLeftUntouched() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("{ Hello {}", "X");
+
+            assertEquals("A { Hello X\n", cap.read());
+        }
+    }
+
+    @Test
+    void escapedBraces_areActuallyEscaped() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("\\{} {}", "X");
+
+            assertEquals("A {} X\n", cap.read());
+        }
+    }
+
+    @Test
+    void placeholderContainingNewlines_isSplitCorrectly() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("{}", "A\nB");
+
+            assertEquals(
+                    """
+                            A A
+                            A B
+                            """,
+                    cap.read()
+            );
+        }
+    }
+
+    @Test
+    void emptyArgument_producesEmptyOutput() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("X{}Y", "");
+
+            assertEquals("A XY\n", cap.read());
+        }
+    }
+
+    @Test
+    void messageContainingLayoutSyntax_isNotInterpreted() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            logger.info("{message} {}", "X");
+
+            assertEquals("A {message} X\n", cap.read());
+        }
+    }
+
+    @Test
+    void largeArgument_isHandledCorrectly() {
+        try (LogCapture cap = new LogCapture()) {
+            configure(cap.ps);
+
+            String big = "X".repeat(10_000);
+            logger.info("{}", big);
+
+            assertEquals("A " + big + "\n", cap.read());
+        }
+    }
+
+    @Test
+    void formattingDoesNotIntroduceAnsiResetInPlainMessage() {
+        try (LogCapture cap = new LogCapture()) {
+            LoggerManager.configure()
+                    .addOutput(cap.ps)
+                    .enableColorCoding(false)
+                    .logMessageLayout("{message}")
+                    .done();
+
+            logger.info("{}", "X");
+
+            assertEquals("X\n", cap.read());
+            assertFalse(cap.read().contains("\u001B"));
+        }
     }
 
 }
